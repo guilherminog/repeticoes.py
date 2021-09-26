533dd596212a08c75d44cb60f485fff19ba1f2a4
Guilhermino Gomes <90943259+Guilhermino-Gomes@users.noreply.github.com>
2021-09-26 13:46:15 -0300

    Add files via upload

---
+++ 10 repetição com numeros pares.py
@@ -0,0 +1,5 @@
+fim = int(input('Digite o útimo número: '))
+x = 0
+while x <= fim:
+    print(x)
+    x = x + 2
---
+++ 9  repetições binárias.py
@@ -0,0 +1,5 @@
+fim = int(input('Digite o último valor: '))
+x = 1
+while x <= fim:
+    print (x)
+    x = ((x + 1) - 1) * 2
